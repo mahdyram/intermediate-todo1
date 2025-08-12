91143aa948657b007a810bd443cb23cd65ec6ab4
mahdi <mahdyramshini@gmail.com>
2025-08-12 22:42:17 +0330

    update source code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import TaskList from "./components/TaskList/TaskList";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "Eating breakfast at 7:00",
-      completed: false,
-      showInput: false,
-    },
-    {
-      id: 2,
-      text: "Studying English part 2",
-      completed: true,
-      showInput: false,
-    },
-    {
-      id: 3,
-      text: "Watching movie for an hour",
-      completed: false,
-      showInput: false,
-    },
-  ]);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos
+      ? JSON.parse(savedTodos)
+      : [
+          {
+            id: 1,
+            text: "Eating breakfast at 7:00",
+            completed: false,
+            showInput: false,
+          },
+          {
+            id: 2,
+            text: "Studying English part 2",
+            completed: true,
+            showInput: false,
+          },
+          {
+            id: 3,
+            text: "Watching movie for an hour",
+            completed: false,
+            showInput: false,
+          },
+        ];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-200 pb-10 pt-10">
